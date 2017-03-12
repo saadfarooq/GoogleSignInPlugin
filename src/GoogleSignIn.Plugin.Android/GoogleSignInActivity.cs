44861960fea397ca1713c147dfe96c5267b425b1
Saad Farooq <sa@dfarooq.com>
2017-03-11 19:37:08 -0500

    Initial commit

---
+++ src/GoogleSignIn.Plugin.Android/GoogleSignInActivity.cs
@@ -0,0 +1,81 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Common.Apis;
+using Android.Gms.Auth.Api;
+using static Android.Gms.Common.Apis.GoogleApiClient;
+using Xamarin.Forms;
+
+namespace GoogleSignIn.Plugin.Android
+{
+    [Activity(Label = "GoogleSignInActivity", Theme = "@android:style/Theme.Translucent")]
+    public class GoogleSignInActivity : Activity, IConnectionCallbacks
+    {
+        private GoogleApiClient googleApiClient;
+        private readonly int RC_SIGN_IN = 10;
+        public const string EXTRA_CLIENT_ID = "GoogleSignIn.Plugin.Android.GoogleSignInActivity.EXTRA_CLIENT_ID";
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(savedInstanceState);
+
+            var clientId = this.Intent.Extras.GetString(EXTRA_CLIENT_ID);
+
+            Console.WriteLine("Requesting client id: {0}", clientId);
+
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+                .RequestServerAuthCode(clientId)
+                .RequestEmail()
+                .Build();
+
+            googleApiClient = new Builder(this)
+                .AddConnectionCallbacks(this)
+                .AddApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .AddOnConnectionFailedListener((result) =>
+                {
+                    Console.WriteLine("Got failure: {0}", result);
+                })
+                .Build();
+            googleApiClient.Connect();
+        }
+
+        public void OnConnected(Bundle connectionHint)
+        {
+            Intent signInIntent = Auth.GoogleSignInApi.GetSignInIntent(googleApiClient);
+            StartActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (requestCode == RC_SIGN_IN)
+            {
+                GoogleSignInResult result = Auth.GoogleSignInApi.GetSignInResultFromIntent(data);
+                if (result.IsSuccess)
+                {
+                    GoogleSignInAccount acct = result.SignInAccount;
+                    Console.WriteLine("Sign in with Google user: {0}", acct);
+                    // Get account information
+                    MessagingCenter.Send(this, "Success", new GoogleSignInUser()
+                    {
+                        ServerAccessToken = acct.ServerAuthCode
+                    });
+                }
+                else
+                {
+                    Console.WriteLine("Sign in with Google failed");
+                }
+            }
+            Finish();
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+
+        }
+    }
+}
---
+++ src/GoogleSignIn.Plugin.iOS/GoogleSignInButtonRenderer.cs
@@ -0,0 +1,60 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using Google.SignIn;
+using Foundation;
+using GoogleSignIn.Plugin;
+using GoogleSignIn.Plugin.iOS;
+
+[assembly: ExportRenderer(typeof(GoogleSignInButton), typeof(GoogleSignInButtonRenderer))]
+namespace GoogleSignIn.Plugin.iOS
+{
+    /// <summary>
+    /// GoogleSignInPlugin Renderer
+    /// </summary>
+    public class GoogleSignInButtonRenderer : ViewRenderer<GoogleSignInButton, UIControl>, ISignInDelegate
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<GoogleSignInButton> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null)
+            {
+                SignIn.SharedInstance.ClientID = this.Element.ClientIdiOS;
+                SignInButton signInButton = new SignInButton();
+
+                switch (this.Element.Size)
+                {
+                    case GoogleSignInButton.SizeOptions.IconOnly:
+                        signInButton.Style = ButtonStyle.IconOnly;
+                        break;
+                    case GoogleSignInButton.SizeOptions.Wide:
+                        signInButton.Style = ButtonStyle.Wide;
+                        break;
+                    default:
+                        signInButton.Style = ButtonStyle.Standard;
+                        break;
+                }
+
+                SetNativeControl(signInButton);
+
+                SignIn.SharedInstance.UIDelegate = new GoogleSignInUIDelegate();
+                SignIn.SharedInstance.Delegate = this;
+            }
+        }
+
+        public void DidSignIn(SignIn signIn, GoogleUser user, NSError error)
+        {
+            if (user != null && error == null)
+            {
+                if (this.Element.Command != null)
+                {
+                    this.Element.Command.Execute(new GoogleSignInUser()
+                    {
+                        ServerAccessToken = user.ServerAuthCode
+                    });
+                }
+            }
+        }
+    }
+}
