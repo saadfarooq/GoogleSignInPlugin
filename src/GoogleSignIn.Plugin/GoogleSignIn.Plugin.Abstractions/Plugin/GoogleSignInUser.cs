cdc0a63412a236936d87c3323ca7792c52335ce7
Saad Farooq <sa@dfarooq.com>
2017-09-25 00:19:29 -0400

    Progress commit

---
+++ src/GoogleSignIn.Plugin/GoogleSignIn.Plugin.Abstractions/Plugin/GoogleSignInUser.cs
@@ -0,0 +1,12 @@
+﻿namespace GoogleSignIn.Plugin
+{
+    public class GoogleSignInUser
+    {
+        public string Token { get; set; }
+
+        public override string ToString()
+        {
+            return "{ \n Token: " + Token + "\n}";
+        }
+    }
+}
